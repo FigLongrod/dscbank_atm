1e91262150e5dc06d7b00bff4bb864a4e5e863ed
Figlongrod <Williamgerardi4@gmail.com>
2020-04-23 00:08:12 +1000

    try to group but offset related bills

--- js/wallet.js
+++ js/wallet.js
@@ -23,14 +23,17 @@ class Cash {
     draw() {
         this.element.innerHTML = "";
         let z = 2;
+        let offs = 0;
         ["100", "50", "20", "10", "5", "2", "1"].forEach(note => {
             for (let i = 0; i < this.notes[note]; i++) {
                 let div = document.createElement("div");
                 div.className = "note note-" + note;
                 div.style.zIndex = z++;
+                div.style.top = `${offs}px`;
                 div.style.transform = `translate(${(Math.random() * 6) - 3}px,${(Math.random() * 6) - 3}px) rotate(${(Math.random() * 6) - 3}deg)`;
                 this.element.appendChild(div);
             }
+            offs += 20;
         });
     }
 }
