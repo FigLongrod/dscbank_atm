922d8ae1e69767f9a7d9318a6c8f98ebb69d06ed
Figlongrod <Williamgerardi4@gmail.com>
2020-04-23 08:52:26 +1000

    trying rotation isntead

--- js/wallet.js
+++ js/wallet.js
@@ -24,16 +24,18 @@ class Cash {
         this.element.innerHTML = "";
         let z = 2;
         let offs = 80;
+        let rot = -10;
         ["100", "50", "20", "10", "5", "2", "1"].forEach(note => {
             for (let i = 0; i < this.notes[note]; i++) {
                 let div = document.createElement("div");
                 div.className = "note note-" + note;
                 div.style.zIndex = z++;
                 div.style.top = `${offs}px`;
-                div.style.transform = `translate(${(Math.random() * 6) - 3}px,${(Math.random() * 6) - 3}px) rotate(${(Math.random() * 6) - 3}deg)`;
+                div.style.transform = `translate(${(Math.random() * 4) - 2}px,${(Math.random() * 4) - 2}px) rotate(${(Math.random() * 4) - 2 + rot}deg)`;
                 this.element.appendChild(div);
             }
-            offs += 40;
+            rot += 8;
+            //offs += 40;
         });
     }
 }
